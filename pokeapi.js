19f046cded46680eac6444d96669882282b90d2e
Thomas Phillips <tpphilli@amazon.com>
2016-02-11 22:18:48 -0800

    Show image after user picks a number

--- pokeapi.js
+++ pokeapi.js
@@ -3,6 +3,8 @@
   var num = prompt("Pick a number");
   $.getJSON("http://pokeapi.co/api/v2/pokemon/"+num, function(pokemon) {
     console.log(pokemon.name);
-    alert("you picked" + pokemon.name);
+    // alert("you picked" + pokemon.name);
+    $('#pokePic').prepend('<img id="pokePic" src="bower_components/pokeapi/data/Pokemon_XY_Sprites/5.png" />');
   })
+
 }())
