3ed3b317949d6f66e46a0ceff0478e2d0207d6a3
Thomas Phillips <tpphilli@amazon.com>
2016-02-12 00:07:24 -0800

    Imported skeleton and prettified this joint a lil bit

--- pokeapi.js
+++ pokeapi.js
@@ -1,10 +1,15 @@
 (function(){
   console.log("hey");
+  // $(function() {
+  //   $( "#pokeNumber" ).dialog();
+  // });
+  // $('#pokeMeta').prepend('<link rel="icon" type="image/png" href="bower_components/pokeapi/data/Pokemon_XY_Sprites/'+3+'.png" />');
   var num = prompt("Pick a number");
   $.getJSON("http://pokeapi.co/api/v2/pokemon/"+num, function(pokemon) {
     console.log(pokemon.name);
-    // alert("you picked" + pokemon.name);
-    // $('#pokePic').prepend('<img id="pokePic" src="5.png" />');
+
+    $('#pokeName').prepend('<h2>'+pokemon.name+'</h2>')
+    innerHTML=pokemon.name;
     $('#pokePic').prepend('<img id="pokePic" src="bower_components/pokeapi/data/Pokemon_XY_Sprites/'+num+'.png" />');
   })
 
